2576b8598e3468933e5785e63bfa7c397b4e757e
Galina Jarkich <Galina.Jarkich@lawpro.ca>
2012-12-03 14:09:34 -0500

    setting egit

---
+++ HelloWorld/src/mypackage/HelloWorld.java
@@ -0,0 +1,24 @@
+package mypackage;
+/**
+ * 
+ */
+
+/**
+ * @author jarkichg Dec 3, 2012
+ * <p>
+ * <b>ca.lawpro..HelloWorld</b>
+ * <p>
+ * <p>
+ * @see  {@link  ca.lawpro.package.class#member  label}
+ */
+public class HelloWorld {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello world");
+    }
+
+}
